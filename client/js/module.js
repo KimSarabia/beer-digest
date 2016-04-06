9b53d1f28b826597dc9634f35c309a85ea2d2d46
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-06 15:14:59 -0700

    user auth

--- client/js/module.js
+++ client/js/module.js
@@ -4,7 +4,9 @@ var app = angular.module('beerApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', { url: '/', templateUrl: '/html/home.html' })
-
+    .state('login', { url: '/', templateUrl: '/html/login.html' })
+    .state('dashboard', {url: "/dashboard", templateUrl: "html/dashboard.html",controller: "profileCtrl"})
+    .state('beer', {url: "/beer", templateUrl: "templates/beer.html", controller: "beerCtrl"})
+  })
   $urlRouterProvider.otherwise('/');
 });
