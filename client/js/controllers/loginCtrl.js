4f5ccf732a6573962edfc371cfdab58b86930bc5
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-06 15:38:39 -0700

    endpoints

--- client/js/controllers/loginCtrl.js
+++ client/js/controllers/loginCtrl.js
@@ -2,13 +2,13 @@
 
 var app = angular.module('beerApp');
 
-app.controller('registerCtrl', function($scope, UserService) {
-  $scope.register = function(user) {
-    UserService.register(user)
+app.controller('loginCtrl', function($scope, UserService) {
+  $scope.login = function(user) {
+    UserService.login(user)
       .then(function(res) {
         console.log('res:', res);
       }, function(err) {
         console.error(err);
       });
-  };
+  }
 });
