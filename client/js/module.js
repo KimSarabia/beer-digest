c5045da9098d32be0ec309c251553a4cb2828c59
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-06 14:25:22 -0700

    updated some ui

--- client/js/module.js
+++ client/js/module.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var app = angular.module('testApp', ['ui.router']);
+var app = angular.module('beerApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', { url: '/', templateUrl: '/html/home.html' })
-    
+
   $urlRouterProvider.otherwise('/');
 });
-
